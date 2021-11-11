917e6343250bdd4cf73bd163c298dc244c3b70a0
Antony <vontonnie@gmail.com>
2021-11-11 19:38:22 +0300

    Annotate Application Class with @HiltAndroidApp

--- app/src/main/java/com/example/android/hilt/LogApplication.kt
+++ app/src/main/java/com/example/android/hilt/LogApplication.kt
@@ -17,7 +17,10 @@
 package com.example.android.hilt
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
+
+@HiltAndroidApp
 class LogApplication : Application() {
 
     lateinit var serviceLocator: ServiceLocator
