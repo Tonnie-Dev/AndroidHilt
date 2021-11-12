4fa6249f247f2a08861e1cd283a32539e424700e
Antony <vontonnie@gmail.com>
2021-11-12 07:48:09 +0300

    Perform Field Injection in the LogsFragment

--- app/src/main/java/com/example/android/hilt/ui/LogsFragment.kt
+++ app/src/main/java/com/example/android/hilt/ui/LogsFragment.kt
@@ -31,16 +31,26 @@ import com.example.android.hilt.data.Log
 import com.example.android.hilt.data.LoggerLocalDataSource
 import com.example.android.hilt.util.DateFormatter
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 /**
  * Fragment that displays the database logs.
  */
 
+
+
+/*To make LogsFragment use Hilt, we have to annotate it with
+@AndroidEntryPoint:*/
+
 @AndroidEntryPoint
 class LogsFragment : Fragment() {
 
-    private lateinit var logger: LoggerLocalDataSource
-    private lateinit var dateFormatter: DateFormatter
+    //fields injected by Hilt cannot be private
+    @Inject lateinit var logger: LoggerLocalDataSource
+    @Inject lateinit var dateFormatter: DateFormatter
+
+
+
 
     private lateinit var recyclerView: RecyclerView
 
@@ -63,14 +73,14 @@ class LogsFragment : Fragment() {
     override fun onAttach(context: Context) {
         super.onAttach(context)
 
-        populateFields(context)
+      //  populateFields(context)
     }
 
-    private fun populateFields(context: Context) {
+    /*private fun populateFields(context: Context) {
         logger = (context.applicationContext as LogApplication).serviceLocator.loggerLocalDataSource
         dateFormatter =
             (context.applicationContext as LogApplication).serviceLocator.provideDateFormatter()
-    }
+    }*/
 
     override fun onResume() {
         super.onResume()
